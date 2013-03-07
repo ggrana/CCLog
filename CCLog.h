e77016a4ddc2a33ce021d62c9578e6dfc5c9d2f8
Gustavo Grana <gustavoggs@gmail.com>
2013-03-07 17:31:36 -0300

    CCLog class initial commit

---
+++ CCLog.h
@@ -0,0 +1,21 @@
+//
+//  CCLog.h
+//  SAGCITCAIMAGEAPP
+//
+//  Created by Gustavo Grana on 3/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifdef DEBUG
+#   define DLog(FORMAT, ...) printf("%s\n",[[NSString stringWithFormat:(@"%s [Line %d] " FORMAT), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__] UTF8String]);
+#else
+#   define DLog(...)
+#endif
+
+#define ALog(FORMAT, ...) printf("%s\n",[[NSString stringWithFormat:(@"%s [Line %d] " FORMAT), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__] UTF8String]);
+
+#ifdef DEBUG
+#   define ULog(FORMAT, ...)  { UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:(@"" FORMAT), ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
+#else
+#   define ULog(...)
+#endif
